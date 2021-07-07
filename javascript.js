177bf98903532a19c16e5636502d59926463aa14
David10092013 <74313269+David10092013@users.noreply.github.com>
2021-07-07 15:39:13 +0300

    Projects

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName('collapsible');
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active');
     var box2 = this.nextElementSibling;
-    if (box2.style.display === "block") {
-      box2.style.display = "none";
+    if (box2.style.display === 'block') {
+      box2.style.display = 'none';
     } else {
-      box2.style.display = "block";
+      box2.style.display = 'block';
     }
   });
 }
